eefc4721a60a4f3f7acf1d882df15c5178a641d4
Cristi Sava <cristi.sava@intelligentbee.com>
2017-08-08 10:08:42 +0300

    - initial commit

---
+++ iBeaconTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iBeaconTest
+//
+//  Created by Cristi Sava on 08/08/2017.
+//  Copyright © 2017 Cristi Sava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
